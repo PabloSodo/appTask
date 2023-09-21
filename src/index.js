1995360aa1d8f725d7a89673753ec623042861ea
Pablo E. Sodo <pablosodo@gmail.com>
2023-09-21 12:36:19 -0300

    general ch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +10,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
